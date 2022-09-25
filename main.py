b7847142a0a1099820132ba86131a1f123162470
Araya2001 <araya2001@outlook.es>
2022-09-25 13:09:59 -0600

    Base Project

---
+++ main.py
@@ -0,0 +1,42 @@
+# 2022-09-21 - Clase Programación Básica
+
+"""
+    Otra forma de ver comentarios
+"""
+# print(7 + 5 - 6)
+# calculo = 9 + 7 * 8 - 36 / 5
+# calculo = (9 + 7) * 8 - 36 / 5
+# print(calculo)
+
+# calculo = 100 * 90 / 10
+# print("El resultado de la operación realizada es", calculo)
+
+# caso1 = 8 * 7 ** 3 / 4 // 3
+# caso2 = 10 / 2 * (15 + (33 - 8 * 2 + (10 * 10 / 4) / 5) / 2) + 10
+#
+# print('Caso 1:', caso1)
+# print('Caso 2:', caso2)
+
+# x = 4
+# b = 2
+# calc1 = (x * 5 + b ** 3 / 4)
+# calc2 = (x ** 3 - 1)
+# calc_bool = calc1 >= calc2
+# print("Cálculo 1:", calc1)
+# print("Cálculo 2:", calc2)
+# print("Cálculo lógico:", calc_bool)
+# print(not (5 > 18 and 18 > 5))
+# print(not (5 > 18 or 18 > 5))
+
+# bool_example = not (5 > 18 or 18 > 5 and (3+2) >= 5)
+# print(bool_example)
+
+# Ejercicio 2:
+# edad = int(input("Edad: ")) + 5
+# print("Su edad en 5 años será", edad)
+
+# Ejercicio 3:
+a = int(input("A: "))
+b = int(input("B: "))
+calc = ((a + b) ** 2) / 3
+print(calc)
---
+++ practica_grupal_1.py
@@ -0,0 +1,19 @@
+
+# Realice  un  programa  que  dada la base y la altura de un  rectángulo, calcule el área y el perímetro de este.
+
+print("Ejercicio 1)")
+base = float(input("Ingrese la base del rectángulo:"))
+altura = float(input("Ingrese la altura del rectángulo:"))
+calc = base * altura
+print("El área del rectángulo es:", calc)
+
+
+# Desarrolle un programa que solicite la distancia de su casa a  la Universidad, el costo por kilómetro, la cantidad
+# de días a la  semana que viaja a la Universidad y que calcule el costo total  de trasladarse por cuatrimestre.
+# Asuma que cada visite implica ida y vuelta y que el  cuatrimestre tiene 15 semanas.
+print("Ejercicio 2)")
+distancia = float(input("Distancia de su casa a la Universidad:"))
+costo_x_kilometro = float(input("Costo por kilómetro del viaje:"))
+cant_dias_viaje = int(input("Cantidad de dias que viaja a la Universidad:"))
+
+

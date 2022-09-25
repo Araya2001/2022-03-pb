979781eb5a59cfcd14011b6d06d924dbc2afca40
Araya2001 <araya2001@outlook.es>
2022-09-25 13:23:30 -0600

    Base Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2022-03-pb
-Programación básica - Cuatrimestre 03 2022
+
+Programación básica - Cuatrimestre 03 2022 - AAJ
+
+## Estilo de Código
+
+Los ejercicios realizados en este proyecto se encuentran alineados a la especificación de Python Enhancement.
+
+Proposals (PEP)Referencia: https://peps.python.org/pep-0008/
--- practica_grupal_1.py
+++ practica_grupal_1.py
@@ -1,5 +1,8 @@
+# Los ejercicios realizados en este documento se encuentran alineados a la especificación de Python Enhancement
+# Proposals (PEP)
+# Referencia: https://peps.python.org/pep-0008/
 
-# Realice  un  programa  que  dada la base y la altura de un  rectángulo, calcule el área y el perímetro de este.
+# Realice un programa que dada la base y la altura de un rectángulo, calcule el área y el perímetro de este.
 
 print("Ejercicio 1)")
 base = float(input("Ingrese la base del rectángulo:"))
@@ -7,13 +10,11 @@ altura = float(input("Ingrese la altura del rectángulo:"))
 calc = base * altura
 print("El área del rectángulo es:", calc)
 
-
-# Desarrolle un programa que solicite la distancia de su casa a  la Universidad, el costo por kilómetro, la cantidad
-# de días a la  semana que viaja a la Universidad y que calcule el costo total  de trasladarse por cuatrimestre.
-# Asuma que cada visite implica ida y vuelta y que el  cuatrimestre tiene 15 semanas.
+# Desarrolle un programa que solicite la distancia de su casa a la Universidad, el costo por kilómetro, la cantidad
+# de días a la semana que viaja a la Universidad y que calcule el costo total de trasladarse por cuatrimestre.
+# Asuma que cada visite implica ida y vuelta y que el cuatrimestre tiene 15 semanas.
 print("Ejercicio 2)")
 distancia = float(input("Distancia de su casa a la Universidad:"))
 costo_x_kilometro = float(input("Costo por kilómetro del viaje:"))
 cant_dias_viaje = int(input("Cantidad de dias que viaja a la Universidad:"))
-
-
+cant_semanas

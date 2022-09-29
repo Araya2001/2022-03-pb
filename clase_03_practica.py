d728891233033c31c4749030a34313a7166c4215
Araya2001 <araya2001@outlook.es>
2022-09-28 18:25:23 -0600

    3rd Week File Creation

---
+++ clase_03_practica.py
@@ -0,0 +1,28 @@
+# 1.
+# Elaborar un programa que muestre un mensaje de bienvenida para una persona.
+# Debe imprimir la información como se muestra a continuación:
+# Bienvenido, estimado Nombre PrimerApellido SegundoApellido.
+
+# 2.
+# ¡Vamos a crear una especie de calculadora simple! El programa calculará el de la suma, la resta,
+# la multiplicación y la división de dos números y mostrará los resultados obtenidos.
+#
+# TODO: Nota - Considere que, si ingresamos un cero como segundo valor, obtendremos un error durante la ejecución del
+# programa. En próximas lecciones veremos cómo controlar este error.
+
+# 3.
+# Elabore un programa que calcule y muestre el área y el perímetro de un cuadrado.
+#
+# TODO: Nota - Para realizar los cálculos requiere conocer la medida del lado del cuadrado.
+
+# 4.
+# Desarrolle un programa intercambie el valor de las edades de Luis y Pedro y muestre las edades luego de
+# realizado el intercambio.
+#
+# TODO: Nota - Considere que las variables solo pueden almacenar un valor a la vez, por lo que el asignar un nuevo valor
+# provocará la pérdida del valor anterior.
+
+# 5.
+# Desarrolle un programa que eleve un número a una potencia.
+#
+# TODO: Nota - Debe utilizar instrucciones de lectura y operaciones aritméticas.

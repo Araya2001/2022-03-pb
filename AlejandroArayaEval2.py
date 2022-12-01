efea49fff6128d4e9d702ae03fc44c1ab333927e
Araya2001 <araya2001@outlook.es>
2022-11-30 19:02:49 -0600

    Evaluación

--- AlejandroArayaEval2.py
+++ AlejandroArayaEval2.py
@@ -1,4 +1,4 @@
-# Caso Único 2
+# Estudio de Caso Práctico - Alejandro Araya
 
 def is_even(grade):
     return grade % 2 == 0
@@ -29,8 +29,7 @@ def is_between_limits(grade):
 def read_student_agg():
     y_n_break_flag = False
     students_index = 0
-    students_agg_message = "Desea agregar las notas de un estudiante? ? [y/n]: "
-    grade_break_flag = False
+    students_agg_message = "Desea agregar la nota final de un estudiante? ? [y/n]: "
     highest_grade = 0
     lowest_grade = 100
     even_grade_count = 0
@@ -39,8 +38,9 @@ def read_student_agg():
     student_has_not_passed_count = 0
     grade = 0
     while not y_n_break_flag:
+        grade_break_flag = False
         if students_index != 0:
-            students_agg_message = "Desea agregar las notas de un estudiante adicional? [y/n]: "
+            students_agg_message = "Desea agregar la nota final de un estudiante adicional? [y/n]: "
         y_n = str(input(students_agg_message))
         if y_n.lower() == "n":
             y_n_break_flag = True
@@ -49,7 +49,7 @@ def read_student_agg():
                 grade = float(input("Ingrese la nota final del estudiante: "))
                 grade_break_flag = is_between_limits(grade)
             highest_grade = get_highest_grade(grade, highest_grade)
-            lowest_grade = get_highest_grade(grade, lowest_grade)
+            lowest_grade = get_lowest_grade(grade, lowest_grade)
             if is_even(grade):
                 even_grade_count += 1
             else:
@@ -59,9 +59,16 @@ def read_student_agg():
             else:
                 student_has_not_passed_count += 1
             students_index += 1
+        else:
+            print("Por favor, ingrese una opción válida")
+    print('\n------------------------------------------------------------------------------')
     print("Estudiantes qué han aprobado el curso:", student_has_passed_count)
     print("Estudiantes qué no han aprobado el curso:", student_has_not_passed_count)
     print("Nota más alta:", highest_grade)
     print("Nota más baja:", lowest_grade)
     print("Cantidad de notas pares:", even_grade_count)
     print("Cantidad de notas impares:", odd_grade_count)
+    print('------------------------------------------------------------------------------')
+
+
+read_student_agg()
